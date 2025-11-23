60bf7fa8c8a5555d2f85af73332661985448b780
GUILLERMO GRILLO GONZALEZ <guillermoEgrillo@outlook.com>
2025-11-23 15:11:22 -0600

    Added API and FrontEnd projects

---
+++ EasyPOS.FrontEnd/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyPOS.FrontEnd
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_QuickSale(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Tickets(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_AccountsReceivable(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_CloseShift(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_TodaySales(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Queries(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Inventory(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Settings(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
